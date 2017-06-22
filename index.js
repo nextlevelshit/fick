8f451d08cecd4872cca62c76fddd397508ca4ae5
Michael Czechowski <mail@dailysh.it>
2017-06-22 17:42:38 +0200

    first file structure setup

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const chalk = require('chalk');
+const clear = require('clear');
+const CLI = require('clui');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const Preferences = require('preferences');
+const _  = require('lodash');
